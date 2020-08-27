45d855cb5706d1059d8cba5811ef4d5ec27ac7d3
unknown <klauder>
2020-08-27 17:48:53 -0300

    Aula 06 - Templates

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+
+  constructor() {
+    this.nomePortal = 'https://klauderdias.com.br';
+   }
 
   ngOnInit(): void {
   }
